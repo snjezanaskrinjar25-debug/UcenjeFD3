6b01970eb73cac05eba82c2a8e7e8e2113138b89
snjezanaskrinjar25-debug <snjezana.skrinjar25@gmail.com>
2026-07-21 18:58:42 +0200

    vjezba06 zad.

---
+++ 03_JS/vjezba06/skripta.js
@@ -0,0 +1,41 @@
+
+
+
+const ocjena = 5
+
+switch(ocjena){
+    case 1 :
+        console.log("Nedovoljan")
+        break
+    case 2 :
+     console.log("Dovoljan")
+            break
+    case 3 :
+        console.log("Dobar")
+        break
+    case 4 :
+        console.log("Vrlo dobar")   
+        break
+    case 5 :
+        console.log("Izvrstan") 
+        break  
+default:
+    console.log("nije ocjena")  
+
+}
+
+
+
+const dan = "Utorak"
+
+switch(dan){
+case "Ponedjeljak":
+case "Utorak":
+case "Srijeda":
+case "Četvrtak":
+case "Petak":
+console.log("Radni dan")
+break
+default:
+    console.log ("Neispravan dan")
+}  
