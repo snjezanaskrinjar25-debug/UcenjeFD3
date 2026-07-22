2a0417a02d2f6bf397a8e494f05c55f730f0f566
snjezanaskrinjar25-debug <snjezana.skrinjar25@gmail.com>
2026-07-22 19:54:12 +0200

    vjezba07js

---
+++ 03_JS/vjezba07/skripta.js
@@ -0,0 +1,115 @@
+
+
+
+
+const t1 = 4
+
+
+const prazanNiz = []
+console.log("prazanNiz, prazanNiz")
+
+
+console.log("prazanNiz.length ,prazanNiz,lenght")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+temp[S = ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const mjesta ={
+    "Osijek"
+    "Zagreb"
+    "Split"
+    "Rijeka"
+    "Dubrovnik"
+    "Zadar"
+
+}
+console.table(mjesta)
+
+
+
+
+
+
+const ptp ={
+    "Edunova",// string
+    7,// number
+    18n, //bigint
+    true,// boolean
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const niz =
